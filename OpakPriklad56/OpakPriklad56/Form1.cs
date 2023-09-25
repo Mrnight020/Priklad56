2f89723b8f1cc16f3e3b624c9837062c32ddcf68
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-09-25 20:12:50 +0200

    Add files via upload

---
+++ OpakPriklad56/OpakPriklad56/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpakPriklad56
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Vek(string radek,ref string mesic,ref bool prvnivprosinci,ref string jmenoprosinec,ref double vek2)
+        {
+            string[] mesice = { "leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec" };
+           
+
+            string[] line = radek.Split(';');
+
+            string rodnycislo = line[2].Substring(0, 6);
+            int cislomesice = (int.Parse(rodnycislo.Substring(2, 2)));
+
+            string skutecnyrok;
+            if(cislomesice > 12)
+            {
+                mesic = mesice[cislomesice - 51];
+            }
+            else
+            {
+                mesic = mesice[cislomesice - 1];
+            }
+            
+
+            if (int.Parse(rodnycislo.Substring(0,2)) <= 20 && int.Parse(rodnycislo.Substring(0, 2)) >= 0 && line[2].Length == 10)
+            {
+                skutecnyrok = "20" + rodnycislo.Substring(0, 2);
+            }
+            else
+            {
+                skutecnyrok = "19" + rodnycislo.Substring(0, 2);
+            }
+            if(cislomesice == 12 || cislomesice == 62 && prvnivprosinci == false)
+            {
+                jmenoprosinec = line[0];
+                prvnivprosinci = true;
+            }
+
+
+            DateTime datumnarozeni = new DateTime(int.Parse(skutecnyrok),(cislomesice > 12)?(cislomesice-51):(cislomesice), int.Parse(rodnycislo.Substring(4, 2)));
+            TimeSpan vek = DateTime.Now - datumnarozeni;
+            
+            //MessageBox.Show("Měsíc : " + mesic + "   věk :" + (int)vek.Days/365.25);
+            label1.Text += "Měsíc : " + mesic + "   věk :" + (int)(vek.Days / 365.25) + "\n";
+            vek2 = vek.Days / 365.25;
+        }
+        void Vek(string radek, ref string mesic, ref double vek2)
+        {
+            string[] mesice = { "leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec" };
+
+
+            string[] line = radek.Split(';');
+
+            string rodnycislo = line[2].Substring(0, 6);
+            int cislomesice = (int.Parse(rodnycislo.Substring(2, 2)));
+
+            string skutecnyrok;
+            if (cislomesice > 12)
+            {
+                mesic = mesice[cislomesice - 51];
+            }
+            else
+            {
+                mesic = mesice[cislomesice - 1];
+            }
+
+
+            if (int.Parse(rodnycislo.Substring(0, 2)) <= 20 && int.Parse(rodnycislo.Substring(0, 2)) >= 0 && line[2].Length == 10)
+            {
+                skutecnyrok = "20" + rodnycislo.Substring(0, 2);
+            }
+            else
+            {
+                skutecnyrok = "19" + rodnycislo.Substring(0, 2);
+            }
+
+
+            DateTime datumnarozeni = new DateTime(int.Parse(skutecnyrok), (cislomesice > 12) ? (cislomesice - 51) : (cislomesice), int.Parse(rodnycislo.Substring(4, 2)));
+            TimeSpan vek = DateTime.Now - datumnarozeni;
+
+            vek2 = vek.Days / 365.25;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            try
+            {
+                double vek2 = 0;
+                string jmenoprosinec = "";
+                bool prvnivprosinci = false;
+                string mesic = "";
+                int soucet = 0;
+                int pocet = 0;
+                StreamReader cti = new StreamReader("rodna_cis.txt");
+
+                while (!cti.EndOfStream)
+                {
+                    string radek = cti.ReadLine();
+                    string[] polozky = radek.Split(';');
+                    listBox1.Items.Add(radek);
+                    Vek(radek, ref mesic, ref prvnivprosinci, ref jmenoprosinec, ref vek2);
+
+                    soucet += int.Parse(polozky[1]);
+                    pocet++;
+                }
+                cti.BaseStream.Position = 0;
+                cti.Close();
+                float prumer = (float)soucet / (float)pocet;
+                MessageBox.Show("Prumer Znamek : " + prumer);
+                MessageBox.Show("Prvni clovek v prosinci narozeny je : " + jmenoprosinec);
+
+                label1.Text = "";
+
+                string[] radky1 = File.ReadAllLines("rodna_cis.txt");
+
+                StreamWriter zapis = new StreamWriter("rodna_cis.txt", false);
+                foreach (string radek in radky1)
+                {
+                    Vek(radek, ref mesic, ref prvnivprosinci, ref jmenoprosinec, ref vek2);
+                    string novyRadek = radek + ";" + (int)vek2;
+                    zapis.WriteLine(novyRadek);
+                    listBox2.Items.Add(novyRadek);
+                }
+                zapis.WriteLine(prumer);
+                listBox2.Items.Add(prumer);
+                zapis.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string mesic = "";
+                double vek = 0;
+                SaveFileDialog uloz = new SaveFileDialog();
+                uloz.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+                uloz.DefaultExt = ".txt";
+
+                if (uloz.ShowDialog() == DialogResult.OK)
+                {
+                    List<string> jmena = new List<string>();
+                    List<int> stari = new List<int>();
+                    List<string> mesice = new List<string>();
+                    List<bool> listbool = new List<bool>();
+
+
+                    string[] radky = listBox1.Items.Cast<string>().ToArray();
+                    foreach (string radek in radky)
+                    {
+                        string[] polozky = radek.Split(';');
+                        if (Convert.ToInt32(polozky[1]) < 3)
+                        {
+                            listbool.Add(true);
+                        }
+                        else
+                        {
+                            listbool.Add(false);
+                        }
+                        jmena.Add(polozky[0]);
+                        Vek(radek, ref mesic, ref vek);
+                        stari.Add(Convert.ToInt32(vek));
+                        mesice.Add(mesic);
+                    }
+
+
+                    StreamWriter zapis = new StreamWriter(uloz.FileName);
+                    for (int i = 0; i < jmena.Count; i++)
+                    {
+                        if (listbool[i] == true)
+                        {
+                            zapis.Write(jmena[i] + ";" + stari[i] + ";" + mesice[i]);
+                            listBox3.Items.Add(jmena[i] + ";" + stari[i] + ";" + mesice[i]);
+                        }
+                    }
+                    zapis.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ OpakPriklad56/OpakPriklad56/Form2.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OpakPriklad56
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        List<double> delka = new List<double>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                StreamReader precti = new StreamReader(openFileDialog1.FileName);
+                while (!precti.EndOfStream)
+                {
+                    double nejvetsi = double.MinValue;
+                    
+                    string radek = precti.ReadLine();
+                    listBox1.Items.Add(radek);
+                    string[] slova = radek.Split(';');
+                    foreach(string slovo in slova) { 
+                        if(slovo.Length > nejvetsi)
+                        {
+                            nejvetsi = slovo.Length;
+                        }
+                    }
+
+                    delka.Add(nejvetsi / 10);
+                }
+                precti.Close();
+            }
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            FileStream filestream1 = new FileStream("cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter zapis = new BinaryWriter(filestream1);
+            zapis.BaseStream.Position = 0;
+            for (int i = 0; i < delka.Count; i++)
+            {
+                zapis.Write(delka[i]);
+            }
+            BinaryReader binaryprecti = new BinaryReader(filestream1);
+            binaryprecti.BaseStream.Position = 0;
+            for (int i = 0; i < delka.Count; i++)
+            {
+                double cislo = binaryprecti.ReadDouble();
+                listBox2.Items.Add(cislo);
+            }
+
+            filestream1.Close();
+            button3.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+            FileStream filestream1 = new FileStream("cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            BinaryReader binaryprecti = new BinaryReader(filestream1);
+            BinaryWriter zapis = new BinaryWriter(filestream1);
+
+            binaryprecti.BaseStream.Position = 0;
+            for (int i = 0; i < delka.Count; i++)
+            {
+                double cislo = binaryprecti.ReadDouble();
+                if(cislo < 0.999)
+                {
+                    zapis.BaseStream.Position -= sizeof(double);
+                    zapis.Write(cislo*10);
+                    cislo *= 10;
+                }
+                listBox3.Items.Add(cislo);
+            }
+
+            filestream1.Close();
+            button4.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox4.Items.Clear();
+            double soucet = 0;
+            int pocet = 0;
+            FileStream filestream1 = new FileStream("cisla.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            BinaryReader binaryprecti = new BinaryReader(filestream1);
+            BinaryWriter zapis = new BinaryWriter(filestream1);
+
+            binaryprecti.BaseStream.Position = 0;
+            for (int i = 0; i < delka.Count; i++)
+            {
+                double cislo = binaryprecti.ReadDouble();
+                if (cislo > 2)
+                {
+                    soucet += cislo;
+                    pocet++;
+                }
+                listBox4.Items.Add(cislo);
+            }
+            if(pocet != 0)
+            {
+                double aritemtickysoucet = soucet / pocet;
+                zapis.Write(aritemtickysoucet);
+                listBox4.Items.Add(aritemtickysoucet);
+            }
+            else
+            {
+                listBox4.Items.Add("nejsou zde cisla vetsi jak 2");
+            }
+            filestream1.Close();
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
